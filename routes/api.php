80ecfe865e5ca5bbc0dc28c81f7036097894025c
alex <alexwindsormusic@gmail.com>
2024-02-21 21:08:41 +0000

    split routes into auth and api as well as web

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
+Non-view routes go here :
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// load index method
+// Route::apiResource('/testone', TestController::class);
+
+// works as web.php
+// Route::get('/testone', [TestController::class, 'one'])->middleware('guest');
