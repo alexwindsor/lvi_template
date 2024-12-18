19d41359c6f3b4573ea6085198474cc6428fec9f
alex <alexwindsormusic@gmail.com>
2024-12-18 22:15:57 +0000

    add auth routes

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,9 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
 const Home = () => import('./Pages/Home.vue')
+const Register = () => import('./Pages/Auth/Register.vue')
+const Login = () => import('./Pages/Auth/Login.vue')
+const Logout = () => import('./Pages/Auth/Logout.vue')
 
 const routes = [
     {
@@ -8,7 +11,21 @@ const routes = [
         name: 'Home',
         component: Home
     },
-
+    {
+        path: '/',
+        name: 'Register',
+        component: Register
+    },
+    {
+        path: '/',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/',
+        name: 'Logout',
+        component: Logout
+    },
 ]
 
 const router = createRouter({
